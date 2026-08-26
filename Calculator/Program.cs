e5c3c0f865f281b4dfb95a9d4b8360c336677910
toka <Tokaabd121@gmail.com>
2026-08-26 10:10:19 +0300

    feat:add basics calc

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Calculator");
+
+Console.WriteLine("write first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("write second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("write operation: ");
+string op = Console.ReadLine();
+
+double result = 0;
+
+switch(op)
+{
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        if (num2 != 0)
+        {
+            result = num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("Error: Division by zero");
+            return;
+        }
+        break;
+    default:
+        Console.WriteLine("Error: Invalid operation");
+        return; 
+}
+
+Console.WriteLine($"Result: {result}");
